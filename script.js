b784962fe49fa8c1c5d50fd6d0f4bf51be744442
kevin <kevinelsen@hotmail.fr>
2018-06-21 16:28:32 +0200

    exo6

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function calcul() {
   // recupération des saisies
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
-  var reg =/^[0-9]+[.][0-9]+$/;
+  var reg =/^(([0-9]+)|([0-9]+[.]{1}[0-9]+))$/;
   // test regex
   if(reg.test(firstNumber) && reg.test(secondNumber)){
-    // ont mets le premier nombre a l'entier inferieur
-    firstNumber = Math.trunc(firstNumber);
     // on affiche la multiplication de firstNumber et secondNumber
-    alert(firstNumber / secondNumber);
+    alert(firstNumber % secondNumber);
   } else {
     alert('FAUX');
   }
 }
+var button = document.getElementById("submit");
+button.onclick = calcul();
